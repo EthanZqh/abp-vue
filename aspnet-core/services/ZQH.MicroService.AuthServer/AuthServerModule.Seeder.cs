b373a2800e0f6e7f67dd44d947762b544186db48
Ethan <zqh_2023vip@126.com>
2024-12-11 00:03:43 +0800

    Submit AuthServer

---
+++ aspnet-core/services/ZQH.MicroService.AuthServer/AuthServerModule.Seeder.cs
@@ -0,0 +1,15 @@
+﻿using ZQH.MicroService.AuthServer.DataSeeder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ZQH.MicroService.AuthServer;
+
+public partial class AuthServerModule
+{
+    private static void ConfigureSeedWorker(IServiceCollection services, bool isDevelopment = false)
+    {
+        if (isDevelopment)
+        {
+            services.AddHostedService<AuthServerDataSeederWorker>();
+        }
+    }
+}
---
+++ aspnet-core/services/ZQH.MicroService.AuthServer/AuthServerModule.cs
@@ -0,0 +1,161 @@
+﻿using ZQH.Abp.Account;
+using ZQH.Abp.AspNetCore.HttpOverrides;
+using ZQH.Abp.AspNetCore.Mvc.Wrapper;
+using ZQH.Abp.AuditLogging.Elasticsearch;
+using ZQH.Abp.Authentication.QQ;
+using ZQH.Abp.Authentication.WeChat;
+using ZQH.Abp.Data.DbMigrator;
+using ZQH.Abp.EventBus.CAP;
+using ZQH.Abp.Identity.AspNetCore.Session;
+using ZQH.Abp.Identity.EntityFrameworkCore;
+using ZQH.Abp.Identity.OrganizaztionUnits;
+using ZQH.Abp.Identity.Session.AspNetCore;
+using ZQH.Abp.Localization.CultureMap;
+using ZQH.Abp.LocalizationManagement.EntityFrameworkCore;
+using ZQH.Abp.OpenIddict.AspNetCore.Session;
+using ZQH.Abp.OpenIddict.LinkUser;
+using ZQH.Abp.OpenIddict.Portal;
+using ZQH.Abp.OpenIddict.Sms;
+using ZQH.Abp.OpenIddict.WeChat;
+using ZQH.Abp.OpenIddict.WeChat.Work;
+using ZQH.Abp.Saas.EntityFrameworkCore;
+using ZQH.Abp.Serilog.Enrichers.Application;
+using ZQH.Abp.Serilog.Enrichers.UniqueId;
+using ZQH.Abp.Sms.Aliyun;
+using ZQH.Platform.EntityFrameworkCore;
+using ZQH.MicroService.AuthServer.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+using Volo.Abp.Account.Web;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.LeptonXLite;
+using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.EntityFrameworkCore.MySQL;
+using Volo.Abp.FeatureManagement.EntityFrameworkCore;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+using Volo.Abp.OpenIddict.EntityFrameworkCore;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.PermissionManagement.Identity;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+
+namespace ZQH.MicroService.AuthServer;
+
+[DependsOn(
+    typeof(AbpSerilogEnrichersApplicationModule),
+    typeof(AbpSerilogEnrichersUniqueIdModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpAccountWebOpenIddictModule),
+    typeof(AbpAccountApplicationModule),
+    typeof(AbpAspNetCoreMvcUiLeptonXLiteThemeModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpCachingStackExchangeRedisModule),
+    typeof(AbpEntityFrameworkCoreMySQLModule),
+    typeof(AbpIdentityEntityFrameworkCoreModule),
+    typeof(AbpIdentityApplicationModule),
+    typeof(AbpIdentityAspNetCoreSessionModule),
+    typeof(AbpOpenIddictAspNetCoreSessionModule),
+    typeof(AbpOpenIddictEntityFrameworkCoreModule),
+    typeof(AbpOpenIddictSmsModule),
+    typeof(AbpOpenIddictWeChatModule),
+    typeof(AbpOpenIddictLinkUserModule),
+    typeof(AbpOpenIddictPortalModule),
+    typeof(AbpOpenIddictWeChatWorkModule),
+    typeof(AbpAuthenticationQQModule),
+    typeof(AbpAuthenticationWeChatModule),
+    typeof(AbpIdentityOrganizaztionUnitsModule),
+    typeof(PlatformEntityFrameworkCoreModule),
+    typeof(AbpLocalizationManagementEntityFrameworkCoreModule),
+    typeof(AbpPermissionManagementDomainIdentityModule),
+    typeof(AbpPermissionManagementEntityFrameworkCoreModule),
+    typeof(AbpSettingManagementEntityFrameworkCoreModule),
+    typeof(AbpFeatureManagementEntityFrameworkCoreModule),
+    typeof(AbpSaasEntityFrameworkCoreModule),
+    typeof(AuthServerMigrationsEntityFrameworkCoreModule),
+    typeof(AbpDataDbMigratorModule),
+    typeof(AbpAuditLoggingElasticsearchModule), // 放在 AbpIdentity 模块之后,避免被覆盖
+    typeof(AbpLocalizationCultureMapModule),
+    typeof(AbpAspNetCoreMvcWrapperModule),
+    typeof(AbpAspNetCoreHttpOverridesModule),
+    typeof(AbpCAPEventBusModule),
+    typeof(AbpAliyunSmsModule)
+    )]
+public partial class AuthServerModule : AbpModule
+{
+    private const string DefaultCorsPolicyName = "Default";
+
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        PreConfigureAuth();
+        PreConfigureFeature();
+        PreForwardedHeaders();
+        PreConfigureApp(configuration);
+        PreConfigureCAP(configuration);
+        PreConfigureCertificate(configuration, hostingEnvironment);
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+
+        ConfigureDbContext();
+        ConfigureCaching(configuration);
+        ConfigureIdentity(configuration);
+        ConfigureVirtualFileSystem();
+        ConfigureFeatureManagement();
+        ConfigureLocalization();
+        ConfigureDataSeeder();
+        ConfigureUrls(configuration);
+        ConfigureTiming(configuration);
+        ConfigureAuditing(configuration);
+        ConfigureMultiTenancy(configuration);
+        ConfigureJsonSerializer(configuration);
+        ConfigureMvc(context.Services, configuration);
+        ConfigureCors(context.Services, configuration);
+        ConfigureOpenTelemetry(context.Services, configuration);
+        ConfigureDistributedLocking(context.Services, configuration);
+        ConfigureSeedWorker(context.Services, hostingEnvironment.IsDevelopment());
+        ConfigureSecurity(context.Services, configuration, hostingEnvironment.IsDevelopment());
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        app.UseForwardedHeaders();
+        app.UseMapRequestLocalization();
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            // app.UseErrorPage();
+            app.UseHsts();
+        }
+        // app.UseHttpsRedirection();
+        app.UseCookiePolicy();
+        app.UseCorrelationId();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseCors(DefaultCorsPolicyName);
+        app.UseAuthentication();
+        app.UseAbpOpenIddictValidation();
+        app.UseMultiTenancy();
+        app.UseAbpSession();
+        app.UseDynamicClaims();
+        app.UseAuthorization();
+        app.UseAuditing();
+        app.UseAbpSerilogEnrichers();
+        app.UseConfiguredEndpoints();
+    }
+}
