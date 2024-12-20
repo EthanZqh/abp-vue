f0e0669ac9bd8d79ac332524251751aa30a17c6c
Ethan <zqh_2023vip@126.com>
2024-12-20 17:59:57 +0800

    数据维护

--- aspnet-core/modules/rules-management/rules-engine/ZQH.Abp.RulesEngineManagement.EntityFrameworkCore/obj/Debug/net8.0/ZQH.Abp.RulesEngineManagement.EntityFrameworkCore.AssemblyInfo.cs
+++ aspnet-core/modules/rules-management/rules-engine/ZQH.Abp.RulesEngineManagement.EntityFrameworkCore/obj/Debug/net8.0/ZQH.Abp.RulesEngineManagement.EntityFrameworkCore.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyFileVersionAttribute("8.2.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.2.1+bcccc5ab13a84e2c0c23a49c7193fc69eb75e477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.2.1+61f3ceccdf987d81763abe59a2707c20489dedfe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZQH.Abp.RulesEngineManagement.EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("8.2.1.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/colinin/abp-next-admin")]
